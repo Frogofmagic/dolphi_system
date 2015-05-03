1db0b69db8a68b5a7b36558f9c9e6c2b1c74d11b
Kirby <frogofmagic@gmail.com>
2015-05-03 21:57:00 +0800

    V1.0.4
    PWM OK, but 9DOF module will fail when moto on :(

--- dolphi_system_main.ino
+++ dolphi_system_main.ino
@@ -1,5 +1,3 @@
-#include <MsTimer2.h>
-
 //Project Name : Dolphi System with Proto-Saber Evolution
 //Start from 2015/04 by Frogofmagic.
 //Pin define : 
@@ -9,9 +7,12 @@
 #include <SoftwareSerial.h>
 #include "mpu.h"
 #include "I2Cdev.h"
+#include <MsTimer2.h>
+#include <TimerOne.h>
 //========================================================================================================//
 #define SYSTEM_DEBUGMODE    1
 #define pinDebug            2
+#define pinMoto             9
 //========================================================================================================//
 
 int iMpuUpdateStatus;
@@ -19,21 +20,24 @@ int iSystem_STATUS = 0;
 int iPackagecounter = 0;
 unsigned long time_last;
 
-
-
-SoftwareSerial mySerial(10, 9); // RX, TX
+SoftwareSerial mySerial(12, 11); // RX, TX
 
 void setup() {
-    Fastwire::setup(800,0);                  //Initial I2C
+    Fastwire::setup(200,0);                            //Initial I2C
     
-    Serial.begin(115200);                    //Initial UART to PC Baud Rate 115200
-    mySerial.begin(115200);                  //Initial UART to BLE Baud Rate 115200
+    Serial.begin(115200);                              //Initial UART to PC Baud Rate 115200
+    mySerial.begin(115200);                            //Initial UART to BLE Baud Rate 115200
     
-    iMpuUpdateStatus = mympu_open(200);      //Initial mpu and read status.
-
-    if(SYSTEM_DEBUGMODE){                    //Show mpu initial result.
-        pinMode(pinDebug, OUTPUT);           //Set D2(pinDebug) = output
-        digitalWrite(pinDebug, LOW);         //Set D2  = Low
+    iMpuUpdateStatus = mympu_open(200);                //Initial mpu and read status.
+    
+    pinMode(pinMoto, OUTPUT);                          //Set pinMoto to output.
+    Timer1.initialize(1000);                           // initialize timer1, and set a 1000ms period
+    Timer1.pwm(9, 869);                                // setup pwm on pin 9, 50% duty cycle
+    Timer1.pwm(10, 512);                               // setup pwm on pin 10, 50% duty cycle  
+    
+    if(SYSTEM_DEBUGMODE){                              //Show mpu initial result.
+        pinMode(pinDebug, OUTPUT);                     //Set D2(pinDebug) = output
+        digitalWrite(pinDebug, LOW);                   //Set D2  = Low
         Serial.print("MPU init: "); 
         Serial.println(iMpuUpdateStatus);
     }	
@@ -41,16 +45,46 @@ void setup() {
     MsTimer2::set(10, Timer2_INTERRUPT);     //10ms period
     MsTimer2::start();
 }
+//========================================================================================================//
 
+int iReadfromBLE[4];
+
+void loop() {
+
+}
+
+//========================================================================================================//
+void Timer2_INTERRUPT(){                //Read MPU Data every interrupt
+    iPackagecounter++;
+    iMpuUpdateStatus = mympu_update();  //run once need about 4ms. 
+    
+    mySerial.write(iPackagecounter);    //sned package number first.
+    
+    for(int i=0; i<3; i++)              //send ypr data to BLE
+        mySerial.write(mympu.ypr[i]); 
+    for(int i=0; i<3; i++)              //send gyr data
+        mySerial.write(mympu.gyro[i]); 
+    
+    if(SYSTEM_DEBUGMODE){               //Show mpu data 
+        Serial.print(" Y: "); Serial.print(mympu.ypr[0]);
+        Serial.print(" P: "); Serial.print(mympu.ypr[1]);
+        Serial.print(" R: "); Serial.print(mympu.ypr[2]);
+        Serial.print("\tgy: "); Serial.print(mympu.gyro[0]);
+        Serial.print(" gp: "); Serial.print(mympu.gyro[1]);
+        Serial.print(" gr: "); Serial.println(mympu.gyro[2]);    
+    }         
+}
+
+//========================================================================================================//
+//========================================================================================================//
+//========================================================================================================//
 /*
 unsigned int c = 0; //cumulative number of successful MPU/DMP reads
 unsigned int np = 0; //cumulative number of MPU/DMP reads that brought no packet back
 unsigned int err_c = 0; //cumulative number of MPU/DMP reads that brought corrupted packet
 unsigned int err_o = 0; //cumulative number of MPU/DMP reads that had overflow bit set
 */
-int iReadfromBLE[4];
-
-void loop() {
+//========================================================================================================//
     /*
     if (mySerial.available()>=4){                     //check data from BLE
         digitalWrite(pinDebug, HIGH);       //Set D2  = HIGH
@@ -103,29 +137,6 @@ void loop() {
     Serial.print(" gp: "); Serial.print(mympu.gyro[1]);
     Serial.print(" gr: "); Serial.println(mympu.gyro[2]);
     */
-
-
-}
-
-
-void Timer2_INTERRUPT(){                //Read MPU Data every interrupt
-    iPackagecounter++;
-    iMpuUpdateStatus = mympu_update();  //run once need about 4ms. 
     
-    mySerial.write(iPackagecounter);    //sned package number first.
     
-    for(int i=0; i<3; i++)              //send ypr data to BLE
-        mySerial.write(mympu.ypr[i]); 
-    for(int i=0; i<3; i++)              //send gyr data
-        mySerial.write(mympu.gyro[i]); 
-    
-    if(SYSTEM_DEBUGMODE){               //Show mpu data 
-        Serial.print(" Y: "); Serial.print(mympu.ypr[0]);
-        Serial.print(" P: "); Serial.print(mympu.ypr[1]);
-        Serial.print(" R: "); Serial.print(mympu.ypr[2]);
-        Serial.print("\tgy: "); Serial.print(mympu.gyro[0]);
-        Serial.print(" gp: "); Serial.print(mympu.gyro[1]);
-        Serial.print(" gr: "); Serial.println(mympu.gyro[2]);    
-    }     
-    
-}
+//========================================================================================================//    
